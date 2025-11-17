379be2308b9d5bca6b8e7b44e91c079753ace363
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-11-17 17:31:04 -0600

    Update extractImage.ts

--- lib/extractImage.ts
+++ lib/extractImage.ts
@@ -1,13 +1,13 @@
-export function extractImageFromTokenUri(uri?: string | null): string | undefined {
+// lib/extractImage.ts
+export function extractImageFromTokenUri(uri?: string | null) {
   if (!uri) return;
 
   if (uri.startsWith("data:application/json")) {
     try {
-      const b64 = uri.split(",")[1];
-      if (!b64) return;
-      const json = JSON.parse(atob(b64));
+      const base64 = uri.split(",")[1];
+      const json = JSON.parse(atob(base64));
       return json?.image;
-    } catch (e) {
+    } catch {
       return;
     }
   }
